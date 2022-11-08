f190e34da11e9d59c615bdc5a72ca67e5a0fafa2
Matosik <boga4iov.matvei2016@yandex.ru>
2022-11-08 17:41:58 +0300

    Еще поправки
    если дискриминант равен 0 передается так же массив, а не аргументы

--- script.py
+++ script.py
@@ -1,8 +1,26 @@
 import sys
 import concurrent.futures
-def dis(a,b,c):
+def dis(a:float,b:float,c:float)-> float:
+    """Function for finding the discriminant
+
+    Args:
+        a (float): coefficient coefficient in front of x^2
+        b (float): coefficient in front of x
+        c (float): free coefficient
+
+    Returns:
+        float: discriminant
+    """
     return b**2 - 4 *a*c
-def roots(mass):
+def roots(mass)->float:
+    """Root Finding Roots of a Quadratic Equation 
+
+    Args:
+        mass (_type_): array with discriminant,  coefficient in front of x and  coefficient in front of x^2
+
+    Returns:
+        float: finished roots
+    """
     discr=mass[0]
     b=mass[2]
     a=mass[1]
@@ -17,7 +35,11 @@ if(__name__=="__main__"):
         diskrim=dis(arr[0],arr[1],arr[2])
         with concurrent.futures.ProcessPoolExecutor() as ex:
             if(diskrim==0):
-                result=roots(diskrim,arr[0],arr[1])
+                arr_for_root=[]
+                arr_for_root.append(diskrim)
+                arr_for_root.append(arr[0])
+                arr_for_root.append(arr[1])
+                result=roots(arr_for_root)
                 print(result)
             elif(diskrim>0):
                 x1=[diskrim**0.5, arr[0], arr[1]]
